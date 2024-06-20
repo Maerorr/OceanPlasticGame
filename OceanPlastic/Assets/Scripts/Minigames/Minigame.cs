0101a125ffbb5c8b75d577889bd478ce0348c23f
Maerorr <7hubert7@gmail.com>
2024-06-20 18:34:03 +0200

    feat: outline shader + minigames tooltip

--- OceanPlastic/Assets/Scripts/Minigames/Minigame.cs
+++ OceanPlastic/Assets/Scripts/Minigames/Minigame.cs
@@ -1,5 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -8,11 +8,25 @@ public abstract class Minigame : MonoBehaviour
     public UnityEvent onWin;
     public UnityEvent onBack;
 
+    public string tooltipText;
+    public TextMeshPro tooltipTMP;
+    private float initialTooltipLocalY;
+
     protected void MinigameInit()
     {
+        tooltipTMP = GameObject.Find("Tooltip").GetComponent<TextMeshPro>();
+        initialTooltipLocalY = tooltipTMP.transform.localPosition.y;
+        tooltipTMP.text = tooltipText;
+        tooltipTMP.color = new Color(1f, 1f, 1f, 0f);
         GetComponentInChildren<NonUIButton>().buttonClicked.AddListener(() =>
         {
             onBack.Invoke();
         });
+        var sq = DOTween.Sequence();
+        sq.SetUpdate(true);
+        sq.Append(tooltipTMP.DOFade(1f, 1.5f).SetEase(Ease.InQuad));
+        sq.Append(tooltipTMP.transform.DOLocalMoveY(initialTooltipLocalY + 6f, 2f).SetDelay(4f).SetEase(Ease.OutQuad).OnComplete(
+            () => tooltipTMP.gameObject.SetActive(false)));
+        sq.Play();
     }
 }
